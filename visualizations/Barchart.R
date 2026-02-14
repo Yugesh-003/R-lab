4405ec450d20cc9afbeb6cc523e69758503054f5
Yugesh <242237@slcs.edu.in>
2026-02-14 10:07:02 +0530

    bug fixes

--- visualizations/Barchart.R
+++ visualizations/Barchart.R
@@ -1,8 +1,23 @@
+# Install and load package
 install.packages("readxl")
 library(readxl)
-#import data from excel
-setwd('F:/R LAB-04')
-data <- read_excel("BDA.xlsx",sheet=4,range="A1:I19")
+
+# Set working directory
+setwd("C:/Users/Admin/Downloads/")
+
+# Read Excel file
+data <- read_excel("BDA.xlsx", sheet = 4, range = "A1:I19")
+
+# View data
 View(data)
-#create a bar chart
-barplot(counts,main="Bar chart of Regional counts of Sales",xlab="Regions",ylab ="Frequency",col="blue",border="black")
+
+# Create counts of regions (categorical data)
+counts <- table(data$Region)
+
+# Create bar chart
+barplot(counts,
+        main = "Bar chart of Regional Counts of Sales",
+        xlab = "Regions",
+        ylab = "Frequency",
+        col = "blue",
+        border = "black")
--- visualizations/scatterplot.r
+++ visualizations/scatterplot.r
@@ -3,16 +3,21 @@ library(readxl)
 
 setwd("C:/Users/Admin/Downloads")
 
-data <- read_excel("BDA.xlsx", sheet = 5, range = "A1:E19")
+data <- read_excel("BDA.xlsx", sheet = 5, range = "A2:E19")
+View(data)
 
 # check names
 colnames(data)
 
+# Check number of rows
+n <- nrow(data)
+
 df_multi <- data.frame(
-  time = data$Years,
+  time = rep(data$Years, 3),
   values = c(data$Sales, data$VC, data$FC),
-  group = rep(c("Sales","VC","FC"), each = 18)
+  group = rep(c("Sales", "VC", "FC"), each = n)
 )
+
 
 plot(data$Years, data$Sales,
      main = "Scatter plot of sales over the years: 2007-24",
